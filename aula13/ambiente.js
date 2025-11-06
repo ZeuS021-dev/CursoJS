0e54f67677e27321b615de5171dab934a68ff49f
ZeuS021-dev <carlosebono1234@gmail.com>
2025-11-06 14:39:24 -0300

    Aula sobre while

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) 
+
+
+
+/*
+MODO CERTO USANDO WHILE:
+var c = 1
+while (c <= 6) {
+console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+MODO ERRADO:
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+console.log(`Passo ${c}`)
+*/
---
+++ aula13/teste.js
@@ -0,0 +1,5 @@
+var c = Number(0)
+while (c <= 10) {
+    console.log(`Número ${c}.`)
+    c++
+}
