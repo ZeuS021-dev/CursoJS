39fa6052db2382177e2ed91de9b3feb4ba471f65
ZeuS021-dev <carlosebono1234@gmail.com>
2025-11-07 19:39:13 -0300

    Exercício contador

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,30 @@
+function contar() {
+    var inicio = document.querySelector('input#n1');
+    var fim = document.querySelector('input#n2');
+    var passo = document.querySelector('input#n3');
+    var res = document.querySelector('div#res');
+    
+    var inicioValor = Number(inicio.value);
+    var fimValor = Number(fim.value);
+    var passoValor = Number(passo.value);
+    
+    if (isNaN(inicioValor) || isNaN(fimValor) || isNaN(passoValor) || passoValor <= 0) {
+        alert('[ERRO] Valor inválido');
+        res.innerHTML = 'Impossível contar!';
+    } else {
+        res.innerHTML = 'Contando:<br>';
+        if (inicioValor < fimValor) {
+            while (inicioValor <= fimValor) {
+                res.innerHTML += inicioValor + ' 👉 ';
+                inicioValor += passoValor;
+            }
+        } else {
+            while (inicioValor >= fimValor) {
+                res.innerHTML += inicioValor + ' 👉 ';
+                inicioValor -= passoValor;
+            }
+        }
+        res.innerHTML += '🏁';
+    }
+}
+
