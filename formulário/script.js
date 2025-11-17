59f0f3ec8b83da4888dcd5916e31ee338fa86f35
ZeuS021-dev <carlosebono1234@gmail.com>
2025-11-17 15:41:40 -0300

    Melhorando o formulário

--- formulário/script.js
+++ formulário/script.js
@@ -1,3 +1,5 @@
+const { Children } = require("react")
+
 function validar() {
     let nome = document.getElementById('nome')
     let email = document.getElementById('email')
@@ -18,9 +20,41 @@ function validar() {
     } else if (data.value.length == 0) {
         alert('[ERRO] Campo "Data de nascimento" é obrigatório.')
     } else {
+        res.style.marginTop = '20px'
+        res.style.padding = '20px'
+        res.style.borderRadius = '10px'
+        res.style.background = 'black'
+        res.style.color = 'white'
+        res.style.wordWrap = 'break-word'
         res.innerHTML = '<p>Tudo ok!</p>'
         let dataFormatada = new Date(data.value).toLocaleDateString('pt-BR');
-        res.innerHTML += `<p>Seu nome é: ${nome.value},<br> Seu email para contato é: ${email.value},<br> Você mora em: ${pais.value},<br> Seu número para contato é: ${num.value},<br> Sua data de nascimento é: ${dataFormatada}.</p>`
+        res.innerHTML += `<p>Seu nome é: ${nome.value} <p>Seu email para contato é: ${email.value}</p> <p>Você mora em: ${pais.value}</p> <p>Seu número para contato é: ${num.value}</p> <p>Sua data de nascimento é: ${dataFormatada}.</p></p>`
+        let botaoconfirm = document.createElement('input')
+        botaoconfirm.type = 'button'
+        botaoconfirm.value = 'Confirmar'
+        res.appendChild(botaoconfirm)
+        let botaocancel = document.createElement('input')
+        botaocancel.type = 'button'
+        botaocancel.value = 'Cancelar'
+        res.appendChild(botaocancel)
+        botaoconfirm.style.background = 'white'
+        botaocancel.style.background = 'white'
+        botaoconfirm.style.width = '145px'
+        botaocancel.style.width = '145px'
+        botaoconfirm.style.alignItems = 'center'
+        botaocancel.style.alignItems = 'center'
+        botaoconfirm.style.background = 'rgb(53, 184, 53)'
+        botaocancel.style.background = 'red'
+        botaoconfirm.style.color = 'white'
+        botaocancel.style.color = 'white'
+        botaoconfirm.style.borderRadius = '7px'
+        botaocancel.style.borderRadius = '7px'
+        botaoconfirm.style.padding = '10px'
+        botaocancel.style.padding = '10px'
+        botaoconfirm.style.marginRight = '17px'
+        botaocancel.style.marginRight = '17px'
+        botaoconfirm.style.marginBottom = '15px'
+        botaocancel.style.marginBottom = '15px'
     }
     
 
