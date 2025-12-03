bdbe02d31243f3b22208d50bc572a1985d6b64de
ZeuS021-dev <carlosebono1234@gmail.com>
2025-12-03 16:00:20 -0300

    Exercício com object, function e switch case

---
+++ exercicios/exercicio02.js
@@ -0,0 +1,65 @@
+/*
+const carro = {
+    marca: 'Ford',
+    modelo: 'Fiesta',
+    cor: 'preto',
+    ligarCarro: function() {
+        console.log(`${this.marca} ${this.modelo} ligado!`)
+    },
+    irPara: function(destino) {
+        if (destino == undefined) {
+            console.log('Destino não informado.')
+        } else {
+            console.log(`Indo para ${destino}.`)
+        }
+    }
+}
+
+carro.ligarCarro()
+carro.irPara()
+*/
+
+/*
+const pessoa = {
+    nome: 'Maria',
+    idade: 25,
+    profissao: 'Engenheira'
+}
+
+if (pessoa.idade >= 18) {
+    console.log(`${pessoa.nome} é maior de idade.`)
+} else {
+    console.log(`${pessoa.nome} é menor de idade.`)
+}
+
+if (pessoa.profissao.length == 0) {
+    console.log('Profissão não definida.')
+} else {
+    console.log(pessoa.profissao)
+}
+*/
+
+/*
+const animal = {
+    tipo: 'cachorro',
+    raca: 'Labrador',
+    cor: 'branco'
+}
+
+switch (animal.tipo) {
+    case 'cachorro':
+        console.log(`É um cachorro da raça ${animal.raca}.`)
+        break
+    case 'gato':
+        console.log(`É um gato da raca ${animal.raca}.`)
+        break
+    case 'pássaro', 'ave':
+        console.log(`É um pássaro da raça ${animal.raca}.`)
+        break
+    case 'peixe':
+        console.log(`É um peixe da raça ${animal.raca}.`)
+        break
+    default:
+        console.log('Animal desconhecido!')
+}
+*/
